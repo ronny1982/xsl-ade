3574e2e171c46a931864587516f86330cc89cb8b
Ronny Wegener <ronny@macbook.local>
2020-02-16 22:42:37 +0100

    UI changes

--- htdocs/mjs/DocumentInfo.mjs
+++ htdocs/mjs/DocumentInfo.mjs
@@ -1,9 +1,3 @@
-function getProperties(file) {
-    return [
-        'properties ...'
-    ];
-}
-
 export default function DocumentInfo(props) {
     return React.createElement('pre', {
         id: 'documentinfo'
